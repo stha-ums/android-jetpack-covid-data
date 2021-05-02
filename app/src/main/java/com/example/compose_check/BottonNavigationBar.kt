b75b57dd72c5ed1ec8a18a64145695810643c4b2
umesh <umesh>
2021-05-02 12:01:41 +0545

    button nav change

--- app/src/main/java/com/example/compose_check/BottonNavigationBar.kt
+++ app/src/main/java/com/example/compose_check/BottonNavigationBar.kt
@@ -2,15 +2,13 @@ package com.example.compose_check
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -20,121 +18,121 @@ import androidx.compose.ui.unit.dp
 fun BottomAppBar(selectedBottomBarIndex: MutableState<Int>) {
 
 
-//    Surface(
-//        elevation = 10.dp,
-//        color = Color.White,
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .height(50.dp)
-//    ) {
-//        Row(
-//            horizontalArrangement = Arrangement.SpaceEvenly,
-//            verticalAlignment = Alignment.CenterVertically,
-//            modifier = Modifier.padding(8.dp)
-//        ) {
-//            Image(
-//                painterResource(
-//                    when (selectedBottomBarIndex.value == 0) {
-//                        true -> R.drawable.home_fill
-//                        false -> R.drawable.home
-//                    }
-//                ),
-//                "This is content description",
-//                alignment = Alignment.Center,
-//                contentScale = ContentScale.Crop,
-//                modifier = Modifier
-//                    .height(height = 20.dp)
-//                    .width(width = 20.dp)
-//                    .clickable {
-//                        selectedBottomBarIndex.value = 0;
-//
-//                    }
-//            )
-//
-//            Image(
-//                painterResource(
-//                    when (selectedBottomBarIndex.value == 1) {
-//                        true -> R.drawable.search_filled
-//                        false -> R.drawable.search
-//                    }
-//                ),
-//                contentDescription = "This is content description",
-//                alignment = Alignment.Center,
-//                contentScale = ContentScale.Crop,
-//                modifier = Modifier
-//                    .height(height = 20.dp)
-//                    .width(width = 20.dp)
-//                    .clickable(
-//                        onClick = {
-//                            selectedBottomBarIndex.value = 1;
-//                        }
-//
-//                    )
-//            )
-//
-//        }
-//
-//
-//    }
+    Surface(
+        elevation = 10.dp,
+        color = Color.White,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Image(
+                painterResource(
+                    when (selectedBottomBarIndex.value == 0) {
+                        true -> R.drawable.home_fill
+                        false -> R.drawable.home
+                    }
+                ),
+                "This is content description",
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(height = 20.dp)
+                    .width(width = 20.dp)
+                    .clickable {
+                        selectedBottomBarIndex.value = 0;
 
-
-    BottomNavigation {
-        BottomNavigationItem(
-            selected = selectedBottomBarIndex.value == 0,
-            onClick = {
-                selectedBottomBarIndex.value = 0
-            },
-            icon = {
-                Image(
-                    painterResource(
-                        when (selectedBottomBarIndex.value == 0) {
-                            true -> R.drawable.home_fill
-                            false -> R.drawable.home
-                        }
-                    ),
-                    "This is content description",
-                    alignment = Alignment.Center,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .height(height = 20.dp)
-                        .width(width = 20.dp)
-                        .clickable {
-                            selectedBottomBarIndex.value = 0
-
-                        }
-                )
-            },
-            label = { Text("Home") },
-        )
-
-        BottomNavigationItem(
-            selected = selectedBottomBarIndex.value == 1,
-            onClick = {
-                selectedBottomBarIndex.value = 0
-            },
-            icon = {
-                Image(
-                    painterResource(
-                        when (selectedBottomBarIndex.value == 1) {
-                            true -> R.drawable.search_filled
-                            false -> R.drawable.search
-                        }
-                    ),
-                    "This is content description",
-                    alignment = Alignment.Center,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .height(height = 20.dp)
-                        .width(width = 20.dp)
-                        .clickable {
-                            selectedBottomBarIndex.value = 1
-
-                        }
-                )
-            },
-            label = { Text("Search") },
-
+                    }
             )
 
+            Image(
+                painterResource(
+                    when (selectedBottomBarIndex.value == 1) {
+                        true -> R.drawable.search_filled
+                        false -> R.drawable.search
+                    }
+                ),
+                contentDescription = "This is content description",
+                alignment = Alignment.Center,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(height = 20.dp)
+                    .width(width = 20.dp)
+                    .clickable(
+                        onClick = {
+                            selectedBottomBarIndex.value = 1;
+                        }
+
+                    )
+            )
+
+        }
+
+
     }
+
+//
+//    BottomNavigation {
+//        BottomNavigationItem(
+//            selected = selectedBottomBarIndex.value == 0,
+//            onClick = {
+//                selectedBottomBarIndex.value = 0
+//            },
+//            icon = {
+//                Image(
+//                    painterResource(
+//                        when (selectedBottomBarIndex.value == 0) {
+//                            true -> R.drawable.home_fill
+//                            false -> R.drawable.home
+//                        }
+//                    ),
+//                    "This is content description",
+//                    alignment = Alignment.Center,
+//                    contentScale = ContentScale.Crop,
+//                    modifier = Modifier
+//                        .height(height = 20.dp)
+//                        .width(width = 20.dp)
+//                        .clickable {
+//                            selectedBottomBarIndex.value = 0
+//
+//                        }
+//                )
+//            },
+//            label = { Text("Home") },
+//        )
+//
+//        BottomNavigationItem(
+//            selected = selectedBottomBarIndex.value == 1,
+//            onClick = {
+//                selectedBottomBarIndex.value = 0
+//            },
+//            icon = {
+//                Image(
+//                    painterResource(
+//                        when (selectedBottomBarIndex.value == 1) {
+//                            true -> R.drawable.search_filled
+//                            false -> R.drawable.search
+//                        }
+//                    ),
+//                    "This is content description",
+//                    alignment = Alignment.Center,
+//                    contentScale = ContentScale.Crop,
+//                    modifier = Modifier
+//                        .height(height = 20.dp)
+//                        .width(width = 20.dp)
+//                        .clickable {
+//                            selectedBottomBarIndex.value = 1
+//
+//                        }
+//                )
+//            },
+//            label = { Text("Search") },
+//
+//            )
+//
+//    }
 }
